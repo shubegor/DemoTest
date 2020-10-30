4137d121520ccb25be8cbed8d25d4fa059171dc6
Егор Шубкин <shub-egor@yandex.ru>
2020-10-30 21:59:22 +0700

    Some ones else feature

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -10,7 +10,7 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello my one more feature");
         }
     }
 }
